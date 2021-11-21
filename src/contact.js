d2c84f0c07f7204095b3a864a97924ca2ec138f0
MmrMahfuj <mahfuj.mmr3@gmail.com>
2021-11-21 19:38:31 +0600

    finished

---
+++ src/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const contact = () => {
+    return (
+        <div>
+            <div className="google-map-code">
+                <iframe src="https://maps.google.com/maps?q=Mirpur%20General%20Hospital&t=&z=13&ie=UTF8&iwloc=&output=embed" width="600" height="450" frameborder="0" style={{ border: 0 }} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+        </div>
+    );
+};
+
+export default contact;
